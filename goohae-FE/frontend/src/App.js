93b97ae399b25916abf239a352509367661f2264
DESKTOP-IQ624AA\yk_Ha <proguy425@gmail.com>
2023-02-16 17:53:10 +0900

    기타 초기설정 수정

--- goohae-FE/frontend/src/App.js
+++ goohae-FE/frontend/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import userData from './instanceData/data';
+
 
 function App() {
+  console.log(userData.id);
+
   return (
     <div className="App">
+
     </div>
   );
 }
---
+++ goohae-FE/frontend/src/instanceData/data.js
@@ -0,0 +1,7 @@
+const userData = {
+    userSeq: 1,
+    id: "kim",
+    password: "1234",
+};
+
+export default userData;
