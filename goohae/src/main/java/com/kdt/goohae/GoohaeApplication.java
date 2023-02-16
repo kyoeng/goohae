cb5d12d655447bcd1e32a49958aaa473b70f9ea6
User -2022BCCGJ\Administrator <rudwlsdn@naver.com>
2023-02-16 14:29:38 +0900

    2023.02.16 spring-boot create

---
+++ goohae/src/main/java/com/kdt/goohae/GoohaeApplication.java
@@ -0,0 +1,13 @@
+package com.kdt.goohae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoohaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoohaeApplication.class, args);
+	}
+
+}
