03a79066c96eed25a57455324ba064cd4eff6307
User -2022BCCGJ\Administrator <rudwlsdn@naver.com>
2023-02-20 17:57:53 +0900

    2023.02.20 npm update

--- goohae-FE/frontend/src/App.js
+++ goohae-FE/frontend/src/App.js
@@ -28,7 +28,7 @@ function App() {
           <Route path='etc' element={<AdminEtc />} />
         </Route>
 
-        <Route path='adminLogin' element={<AdminLogin />} />
+        <Route path='/adminLogin' element={<AdminLogin />} />
 
 
 
