9f32208dc7d498e5bf10d27b7235e547d7b631cf
DESKTOP-IQ624AA\yk_Ha <proguy425@gmail.com>
2023-03-02 12:41:03 +0900

    instance commit

--- goohae-FE/frontend/src/components/presentational/admin/adminPtable.js
+++ goohae-FE/frontend/src/components/presentational/admin/adminPtable.js
@@ -1,11 +1,15 @@
 import adminPtableStyled from '../../CSS/admin/adminPtable.module.css';
-import TableHeader from 'tableHeader';
+import TableHeader from '../admin/tableHeader';
 
 
 export default function AdminPtable() {
     return (
         <div className={adminPtableStyled.pTableContainer}>
-
+            <TableHeader
+                header1='제품명'
+                header2='가격'
+                header3='원가'
+                header4='제품아이디' />
         </div>
     );
 }
--- goohae-FE/frontend/src/pages/presentational/admin/adminProduct.js
+++ goohae-FE/frontend/src/pages/presentational/admin/adminProduct.js
@@ -3,6 +3,8 @@ import adminPstyled from '../../css/admin/adminProduct.module.css';
 import TitleContainer from './../../../components/presentational/admin/titleContainer';
 import AdminPstatusBox from '../../../components/presentational/admin/adminPstatusBox';
 import AdminPsearchBar from '../../../components/presentational/admin/adminPsearchBar';
+import AdminPtable from '../../../components/presentational/admin/adminPtable';
+
 
 import { StatusArr, ProdData } from '../../../components/container/admin/adminPstatusBox';
 
@@ -20,6 +22,7 @@ export default function AdminProduct() {
 
             <AdminPsearchBar />
 
+            <AdminPtable />
         </div>
     );
 }
