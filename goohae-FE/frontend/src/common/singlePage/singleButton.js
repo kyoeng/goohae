c53f21fd608b08513017eb47457045578c703711
dotory08 <purgpurg08@gmail.com>
2023-03-07 12:02:57 +0900

    싱글버튼수정

--- goohae-FE/frontend/src/common/singlePage/singleButton.js
+++ goohae-FE/frontend/src/common/singlePage/singleButton.js
@@ -1,40 +1,20 @@
 import React from 'react';
-import styled from 'styled-components';
+import styles from '../CSS/singlePage/singleButton.module.css';
 // import oc from 'open-color';
 // import { shadow } from 'lib/styleUtils';
-import { useDispatch } from 'react-redux';
-import { findPassword } from '../../stores/store/slice/singlePage/findPwSlice';
-
-const Button = styled.button`
-    display: block;
-    width: 320px;
-    height: 36px;
-    line-height: 36px;
-    text-align:center;
-    margin: 10px auto 18px;
-    border: 0;
-    color: white;
-    font-size: 1.4rem;
-    cursor: pointer;
-    background:#9497AB;
-    
-    &:hover {
-        background-color: #505DD0;
-    }
-`;
+// import { useDispatch } from 'react-redux';
+// import { findPassword } from '../../stores/store/slice/singlePage/findPwSlice';
 
 // 사용 : <SingleButton children="버튼이름"/>
 
 
 const SingleButton = ({ children, ...rest }) => {
-    const dispatch = useDispatch();
-
-
+    // const dispatch = useDispatch();
 
     return (
-        <Button {...rest}>
+        <button {...rest} className={styles.singlePageButton}>
             {children}
-        </Button>
+        </button>
     );
 };
 
